d827b47ee27da3d8f7766699525865e69cb1ced6
Isaac <uniqucode@gmail.com>
2022-07-24 04:55:37 +0100

    Refrencing new quote

--- js/script.js
+++ js/script.js
@@ -2,3 +2,4 @@ const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
