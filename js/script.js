9adbe757aa2061965463410cf00aa7e5cb001548
Isaac <uniqucode@gmail.com>
2022-07-26 05:07:24 +0100

    Added await fetch for Urls

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ async function getQuote() {
   const proxyUrl = 'https://whispering-tor-04671.herokuapp.com/';
   const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
   try {
-      
+      const response = await fetch(proxyUrl + apiUrl);
   } catch {
 
   }
