e31ec618c8b29bf0117f5b5ad15d5a8dfd12294d
Isaac <uniqucode@gmail.com>
2022-07-26 05:10:51 +0100

    Added await response from Urls

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ async function getQuote() {
   const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
   try {
       const response = await fetch(proxyUrl + apiUrl);
+      const data = await response.json();
   } catch {
 
   }
