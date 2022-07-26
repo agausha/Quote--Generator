7a3b8c6c2b1994e9e5c3dc65cd7b86e749cb1e2a
Isaac <uniqucode@gmail.com>
2022-07-26 07:04:57 +0100

    Called removeloadingspinner function

--- js/script.js
+++ js/script.js
@@ -40,6 +40,8 @@ async function getQuote() {
         quoteText.classList.remove('long-quote');
       }
       quoteText.textContent = data.quoteText;
+      // Stop Loader, Show Quote
+      removeLoadingSpinner();
   } catch (error) {
       getQuote();
   }
