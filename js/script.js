a4c5b1d38fcf7c17fc1155152d01b21b708e2801
Isaac <uniqucode@gmail.com>
2022-07-26 05:02:52 +0100

    Added try & catch errors

--- js/script.js
+++ js/script.js
@@ -9,4 +9,9 @@ const loader = document.getElementById('loader');
 async function getQuote() {
   const proxyUrl = 'https://whispering-tor-04671.herokuapp.com/';
   const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+  try {
+      
+  } catch {
+
+  }
 }
