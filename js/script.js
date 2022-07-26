22721d277d23dde77f09ac775fdf2827122cf360
Isaac <uniqucode@gmail.com>
2022-07-26 04:59:21 +0100

    Added apiUrl

--- js/script.js
+++ js/script.js
@@ -7,5 +7,6 @@ const loader = document.getElementById('loader');
 
 // Get Quote from API
 async function getQuote() {
-  const proxyUrl = 'https://whispering-tor-04671.herokuapp.com/'
+  const proxyUrl = 'https://whispering-tor-04671.herokuapp.com/';
+  const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
 }
